43abef8e811725e4c8041e8e2a8899d5235bad0e
Estuardomomo <estuardocorreos@gmail.com>
2019-06-10 00:20:37 -0600

    Get-Post
    La vista ya publica y obtiene datos de la bdd

--- App/src/models/task.js
+++ App/src/models/task.js
@@ -2,15 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
-    Nombre: String,
-    Descripcion: String,
-    Ingredientes: String,
-    TipoMasa: String,
-    Porciones: String,
-    QuesoExtra: {
-        type: Boolean,
-        default: false
-    }
+    nombre: String,
+    descripcion: String,
+    Ingredientes: Array,
+    masa: String,
+    Porciones: Number
 });
 
 module.exports = mongoose.model('tasks',TaskSchema)
--- App/src/routes/index.js
+++ App/src/routes/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const router = express.Router();
-//const Task = require('../models/task');
+const Task = require('../models/task');
 
-router.get('/', (req,res) => {
-    res.render('index');
+//Obtener de la base de datos.
+router.get('/', async (req,res) => {
+    const tasks = await Task.find();
+    console.log(tasks);
+    res.render('index', {
+        tasks
+    });
 });
-
-router.post('/add', (req, res) => {
-    //const task = new Task(req.body);
-    //await task.save();
-    console.log(req.body);
-    res.send('recibido');
+//Insertar en la base de datos.
+router.post('/add', async (req, res) => {
+    const task = new Task(req.body);
+    await task.save();
+    res.redirect('/')
 });
 
 module.exports = router;
